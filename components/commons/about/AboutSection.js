54d372a08a2614c70198a848dc620521596a3b5d
Gerald Heng <geralddevelop@gmail.com>
2021-12-18 15:33:33 +0800

    Clean up

---
+++ components/commons/about/AboutSection.js
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Tilt from "../Tilt";
+import Button from "../Button";
+import LanguageListItem from "./LanguageListItem";
+
+const AboutSection = ({ children, ...rest }) => {
+  const languages = [
+    "Javascript",
+    "React",
+    "Python",
+    "Flutter",
+    "Android",
+    "Vue",
+  ];
+
+  return (
+    <div className="grid grid-cols-2 md:px-40 px-12 md:pt-20 pt-8 gap-12">
+      <div className="flex flex-col gap-8">
+        <div>
+          <h2 className="text-primary">About Me.</h2>
+          <Image
+            src="/images/title_line.svg"
+            height={6}
+            width={125}
+            alt="hover_line"
+          />
+        </div>
+
+        <div>
+          <p className="text-primary">
+            Hello! My name is Gerald and I and a undergraduate in Singapore! My
+            interest for software sparked back in 2015 where I was tasked to
+            create an application for myself in one of my polytechnic module.
+            Now I absolutely love creating new things for people and eager to
+            take on more projects.
+          </p>
+          <br />
+          <p className="text-primary">
+            My main focus these days is to understand the basics of software
+            architecture to built long lasting and efficient codebase and been
+            experimenting with different backend framework as well.
+          </p>
+          <br />
+          <p className="text-primary">
+            Here are the few technologies I am familiar with:
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            {languages.map((language, i) => (
+              <div key={i}>
+                <Tilt>
+                  <LanguageListItem name={language} />
+                </Tilt>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <Image
+          src="/images/line_motion.svg"
+          height={360}
+          width={360}
+          alt="happy face"
+        />
+        <Button>Find Me on Linkedin</Button>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
 import Image from "next/image";
 import HeroSection from "../components/hero/HeroSection";
 import Navbar from "../components/navbar/Navbar";
+import Button from "../components/commons/Button";
+import Tilt from "../components/commons/Tilt";
+import AboutSection from "../components/commons/about/AboutSection";
 
 export default function Home() {
+  const content = {
+    name: "Hi I am Gerald Heng!",
+    title:
+      'I <span class="text-accent">design and build</span> things for the internet.',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et leo nunc. Nulla consequat lorem non volutpat pulvinar. Maecenas vestibulum lacus nec dui gravida, ut molestie orci placerat. Morbi at ex congue, tincidunt quam malesuada",
+  };
+
   return (
-    <div>
+    <div className="pb-40">
       <HeroSection>
         <Navbar />
         <div className="grid grid-cols-1 md:grid-cols-3 md:px-40 px-12 md:pt-20 pt-8 gap-12">
           <div className="flex flex-col justify-center col-span-2">
-            <h4>Hi I am Gerald Heng!</h4>
-            <h1 className="font-black">
-              I <span className="text-accent">design and build</span> things for
-              the internet.
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et leo
-              nunc. Nulla consequat lorem non volutpat pulvinar. Maecenas
-              vestibulum lacus nec dui gravida, ut molestie orci placerat. Morbi
-              at ex congue, tincidunt quam malesuada
-            </p>
+            <h4>{content.name}</h4>
+            <h1
+              className="font-black"
+              dangerouslySetInnerHTML={{ __html: content.title }}
+            />
+            <p>{content.description}</p>
           </div>
           <div className="flex justify-center col-span-1 order-first md:order-last">
             <Image
@@ -31,8 +37,7 @@ export default function Home() {
           </div>
         </div>
       </HeroSection>
-
-      <div>Second section</div>
+      <AboutSection />
     </div>
   );
 }
