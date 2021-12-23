7896862e26d59fe26e82b8dbef21e758ab4ebdf5
Gerald Heng <geralddevelop@gmail.com>
2021-12-23 21:55:39 +0800

    save first

--- components/hero/HeroSection.js
+++ components/hero/HeroSection.js
@@ -1,7 +1,37 @@
 import Image from "next/image";
 
 const HeroSection = ({ children, ...rest }) => {
-  return <div className="flex flex-col hero ">{children}</div>;
+  const content = {
+    name: "Hi I am Gerald Heng!",
+    title:
+      'I <span class="text-accent">design and build</span> things for the internet.',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et leo nunc. Nulla consequat lorem non volutpat pulvinar. Maecenas vestibulum lacus nec dui gravida, ut molestie orci placerat. Morbi at ex congue, tincidunt quam malesuada",
+  };
+
+  return (
+    <div className="flex flex-col hero">
+      {children}
+      <div className="grid grid-cols-1 md:grid-cols-3 md:px-40 px-12 md:pt-20 pt-8 gap-12">
+        <div className="flex flex-col justify-center col-span-2">
+          <h4>{content.name}</h4>
+          <h1
+            className="font-black"
+            dangerouslySetInnerHTML={{ __html: content.title }}
+          />
+          <p>{content.description}</p>
+        </div>
+        <div className="flex justify-center col-span-1 order-first md:order-last">
+          <Image
+            src="/images/happy_face.svg"
+            height={280}
+            width={280}
+            alt="happy face"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,12 @@
-import Image from "next/image";
 import HeroSection from "../components/hero/HeroSection";
 import Navbar from "../components/navbar/Navbar";
-import Button from "../components/commons/Button";
-import Tilt from "../components/commons/Tilt";
 import AboutSection from "../components/commons/about/AboutSection";
 
 export default function Home() {
-  const content = {
-    name: "Hi I am Gerald Heng!",
-    title:
-      'I <span class="text-accent">design and build</span> things for the internet.',
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et leo nunc. Nulla consequat lorem non volutpat pulvinar. Maecenas vestibulum lacus nec dui gravida, ut molestie orci placerat. Morbi at ex congue, tincidunt quam malesuada",
-  };
-
   return (
     <div className="pb-40">
       <HeroSection>
         <Navbar />
-        <div className="grid grid-cols-1 md:grid-cols-3 md:px-40 px-12 md:pt-20 pt-8 gap-12">
-          <div className="flex flex-col justify-center col-span-2">
-            <h4>{content.name}</h4>
-            <h1
-              className="font-black"
-              dangerouslySetInnerHTML={{ __html: content.title }}
-            />
-            <p>{content.description}</p>
-          </div>
-          <div className="flex justify-center col-span-1 order-first md:order-last">
-            <Image
-              src="/images/happy_face.svg"
-              height={280}
-              width={280}
-              alt="happy face"
-            />
-          </div>
-        </div>
       </HeroSection>
       <AboutSection />
     </div>
